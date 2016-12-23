dce947bf5015fb89fc15eda8b8c7fd8bbb69dba6
William Hemmingsen <william.hemmingsen@yahoo.com>
2016-12-23 08:37:10 -0500

    Updated to use any pins for DAC/LED/ADC, do not need to be in order.

--- LibAPRS/device.h
+++ LibAPRS/device.h
@@ -5,7 +5,7 @@
 
 // CPU settings
 #ifndef TARGET_CPU
-    #define TARGET_CPU m328p
+    #define TARGET_CPU m2560
 #endif
 
 #ifndef F_CPU
@@ -23,10 +23,29 @@
 #if TARGET_CPU == m328p
     #define DAC_PORT PORTD
     #define DAC_DDR  DDRD
+    #define DAC_PINS (_BV(4)| _BV(5) | _BV(6) | _BV(7))
     #define LED_PORT PORTB
     #define LED_DDR  DDRB
+    #define LED_RX_PIN 1
+    #define LED_TX_PIN 1
     #define ADC_PORT PORTC
     #define ADC_DDR  DDRC
+    #define ADC_PIN 0
+    #define PPT_PIN 3
 #endif
+#if TARGET_CPU == m2560
+    #define DAC_PORT PORTF
+    #define DAC_DDR  DDRF
+    #define DAC_PINS (_BV(4)| _BV(5) | _BV(6) | _BV(7))
+    #define LED_PORT PORTA
+    #define LED_DDR  DDRA
+    #define LED_RX_PIN 4
+    #define LED_TX_PIN 5
+    #define ADC_PORT PORTF
+    #define ADC_DDR  DDRF
+    #define ADC_PIN 0
+    #define PPT_PIN 3
+#endif
+
 
 #endif
