9c9ccc119f05d2347b20be03bc2d4713458d0b93
jstjst <jonas@gappert.de>
2018-11-21 21:45:25 +0100

    Added serial Stream object

--- src/LibAPRS.h
+++ src/LibAPRS.h
@@ -34,6 +34,6 @@ void APRS_sendLoc(void *_buffer, size_t length);
 void APRS_sendMsg(void *_buffer, size_t length);
 void APRS_msgRetry();
 
-void APRS_printSettings();
+void APRS_printSettings(Stream &serial);
 
 int freeMemory();
